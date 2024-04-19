cf0b717c74a296670e0d6a1452ab2be8960cb6fd
Terje Sylvarnes <fuzzklang@gmail.com>
2024-04-19 19:32:36 +0200

    add ottava indicators

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,72 @@
+import abjad
 from liquid_primes.pitches import generate_pitch_palette
 from liquid_primes.primes import primes
-from liquid_primes.score import partition_notes_to_ranges, show_score
+from liquid_primes.score import (
+    create_voice_with_ottava,
+    show_score,
+)
 
 MIDI_PITCH_RANGE = (-39, 48)  # Piano range in Adjab terms
 CENTRAL_A = 7  # in abjad, midi-pitch 69
 
-ranges = {
-    range(-39, -27): "15vb",
-    range(-27, -15): "8vb",
-    range(-15, 23): "ord",
-    range(23, 49): "8va",
-}
+
+test_palette = (
+    [
+        -39,
+        -29,
+        -25,
+        -16,
+        -14,
+        -10,
+        -5,
+        0,
+        1,
+        2,
+        3,
+        -39,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        12,
+        19,
+        23,
+        24,
+        40,
+        48,
+    ],
+)
+
+
+def test():
+    string = "d'8 f' a' d'' f'' gs'4 r8 e' gs' b' e'' gs'' a'4"
+    voice = abjad.Voice(string, name="RH_Voice")
+    staff = abjad.Staff([voice], name="RH_Staff")
+    score = abjad.Score([staff], name="Score")
+
+    key_signature = abjad.KeySignature(
+        abjad.NamedPitchClass("g"), abjad.Mode("major")
+    )
+    abjad.attach(key_signature, voice[0])
+
+    time_signature = abjad.TimeSignature((2, 4), partial=abjad.Duration(1, 8))
+    abjad.attach(time_signature, voice[0])
+
+    ottava = abjad.Ottava(n=-2, site="after")
+    abjad.attach(ottava, voice[1])
+
+    ottava = abjad.Ottava(n=0, site="after")
+    abjad.attach(ottava, voice[5])
+
+    ottava = abjad.Ottava(n=1, site="after")
+    abjad.attach(ottava, voice[8])
+
+    articulation = abjad.Articulation("turn")
+    abjad.attach(articulation, voice[5])
+
+    abjad.show(score)
 
 
 def main():
@@ -25,39 +81,14 @@ def main():
     palette = generate_pitch_palette(
         reference_pitch, prime_intervals, min_pitch, max_pitch
     )
+    # staff = create_voice_in_staff(palette)
 
-    partition = partition_notes_to_ranges(
-        [
-            -39,
-            -29,
-            -25,
-            -16,
-            -14,
-            -10,
-            -5,
-            0,
-            1,
-            2,
-            3,
-            -39,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            12,
-            19,
-            23,
-            24,
-            40,
-            48,
-        ],
-        ranges,
-    )
-    print(partition)
-    print(palette)
-    show_score(palette)
+    print(f"{palette}")
+    voice = create_voice_with_ottava(palette)
+
+    show_score(abjad.Staff([voice], name="default_staff"))
+
+    # test()
 
 
 if __name__ == "__main__":
--- src/liquid_primes/score.py
+++ src/liquid_primes/score.py
@@ -1,28 +1,22 @@
-from typing import Callable, Dict, List, Tuple
+from typing import Dict, List, Tuple
 import abjad
 
 
-def show_score(pitches: List[int]) -> None:
-    notes = [abjad.NamedPitch(n) for n in pitches]
-    if not notes:
-        return
-    note_string = notes[0].name + "1"
-    note_string += " ".join([n.name for n in notes[1:]]) + "1"
-    # string = "c'16 f' g' a' d' g' a' b' e' a' b' c'' f' b' c'' d''16"
-    voice_1 = abjad.Voice(note_string, name="Voice_1")
-    staff_1 = abjad.Staff([voice_1], name="Staff_1")
-    abjad.show(staff_1)
+def show_score(staff: abjad.Staff) -> None:
+    abjad.show(staff)
 
 
-def in_range_cmp(a: int, b: int) -> Callable[[int], bool]:
-    def f(x: int):
-        return a <= x and x < b
-
-    return f
+ranges = {
+    range(-39, -17): -2,
+    range(-17, -5): -1,
+    range(-5, 23): 0,
+    range(23, 35): 1,
+    range(35, 48): 2,
+}
 
 
 def previous_is_in_same_range(
-    partition: List[Tuple[str, List[str]]], key: str
+    partition: List[Tuple[int, List[int]]], key: int
 ) -> bool:
     if not partition:
         return False
@@ -30,15 +24,70 @@ def previous_is_in_same_range(
 
 
 def partition_notes_to_ranges(
-    notes: List[int], ranges: Dict[range, str]
-) -> List[Tuple[str, List[str]]]:
-    partition: List[Tuple[str, List[str]]] = []
+    notes: List[int], ranges: Dict[range, int]
+) -> List[Tuple[int, List[int]]]:
+    partition: List[Tuple[int, List[int]]] = []
     for n in notes:
         for r in ranges:
             if n in r:
                 if previous_is_in_same_range(partition, ranges[r]):
-                    partition[-1][1].append(str(n))
+                    partition[-1][1].append(n)
                 else:
-                    partition.append((ranges[r], [str(n)]))
+                    partition.append((ranges[r], [n]))
                 continue
     return partition
+
+
+def create_voice_in_staff(
+    pitches: List[int],
+    voice_name: str = "Default voice",
+    staff_name: str = "Default staff",
+    durations: List[int] | None = None,
+) -> abjad.Staff:
+    if not pitches:
+        return abjad.Staff([], name=staff_name)
+    if durations is None:
+        durations = [1]
+    duration_str = f"{str(durations[0])}"
+    notes = [abjad.NamedPitch(n) for n in pitches]
+    voice_str = make_voice_string(notes, duration_str)
+    voice = abjad.Voice(voice_str, name=voice_name)
+    staff = abjad.Staff([voice], name=staff_name)
+    return staff
+
+
+def make_voice_string(notes: List[abjad.NamedPitch], duration_str: str) -> str:
+    voice_string = notes[0].name + duration_str
+    voice_string += " ".join([n.name for n in notes[1:]]) + duration_str
+    return voice_string
+
+
+def create_voice_with_ottava(
+    pitches: List[int],
+    voice_name: str = "Default voice",
+    durations: List[int] | None = None,
+) -> abjad.Voice:
+    if durations is None:
+        durations = [1]
+    duration_str = f"{str(durations[0])}"
+    notes = [abjad.NamedPitch(n) for n in pitches]
+    voice_string = notes[0].name + duration_str
+    voice_string += " ".join([n.name for n in notes[1:]]) + duration_str
+    voice = abjad.Voice(voice_string, name=voice_name)
+
+    for idx, n in get_partition_indices_and_ottava_n(pitches):
+        abjad.attach(abjad.Ottava(n=n, site="before"), voice[idx])
+
+    return voice
+
+
+def get_partition_indices_and_ottava_n(
+    pitches: List[int],
+) -> List[Tuple[int, int]]:
+    indices_and_ottava_n = []
+    idx = 0
+    partitions = partition_notes_to_ranges(pitches, ranges)
+    for n, notes in partitions:
+        indices_and_ottava_n.append((idx, n))
+        idx += len(notes)
+    return indices_and_ottava_n
